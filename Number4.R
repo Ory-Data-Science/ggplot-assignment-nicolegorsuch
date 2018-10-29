998d02780870c2bb87793a9daea04c878d6d266a
nicolegorsuch <42544177+nicolegorsuch@users.noreply.github.com>
2018-10-29 08:30:11 -0500

    Add files via upload
    Number4

--- Number4.R
+++ Number4.R
@@ -6,3 +6,28 @@ dat <- read.csv(url("http://www.esapubs.org/archive/ecol/E088/096/avian_ssd_jan0
                 sep = "\t", na.strings = c("-999.00", "999.00", "-999", "999"))
 
 #your code goes here
+
+
+ggplot(data= dat, mapping = aes(x=F_mass))+
+  geom_histogram()+
+  labs(x="Female Mass(g)")
+
+ggplot(data= dat, mapping = aes(x=log(F_mass), fill="Blue"))+
+  geom_histogram()+
+  labs(x="Female Mass(g)")
+
+ggplot(data= dat, mapping = aes(x=log(F_mass)))+
+           geom_histogram(alpha=0.3)+
+           labs(x="Female Mass(g)")+
+    geom_histogram(mapping=aes(x=log(M_mass)))+
+    facet_wrap(~Family)
+  
+ggplot(data= dat, mapping = aes(x=log(F_wing)))+
+    geom_histogram(alpha=0.3)+
+    labs(x="Wing Size")+
+    geom_histogram(mapping=aes(x=log(M_wing)))+
+    facet_wrap(~Family)
+  
+  
+
+
