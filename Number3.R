9e5278bbf3826deff718feb7abb31a74dcf78557
nicolegorsuch <42544177+nicolegorsuch@users.noreply.github.com>
2018-10-29 08:29:53 -0500

    Add files via upload
    Number 3

--- Number3.R
+++ Number3.R
@@ -6,3 +6,38 @@ dat <- read.csv(url("http://esapubs.org/archive/ecol/E084/093/Mammal_lifehistori
                 sep = "\t", nrows = 1440, na.strings = c("-999.00", "999.00"))
 
 #rest of your code goes here
+masses <- dat%>%
+  group_by(mass.g., newborn.g.)
+
+ggplot(data= masses, aes(x=mass.g., y= newborn.g.))+
+  geom_point()+
+  labs(x="Adult Mass", y="Newborn Mass")
+
+ggplot(data= masses, aes(x=mass.g., y= newborn.g.))+
+  geom_point()+
+  labs(x="Log(Adult Mass)", y="Log(Newborn Mass)")+
+  scale_x_log10()+
+  scale_y_log10()
+
+ggplot(data= masses, aes(x=mass.g., y= newborn.g., color =order))+
+  geom_point()+
+  labs(x="Log(Adult Mass)", y="Log(Newborn Mass)")+
+  scale_x_log10()+
+  scale_y_log10()
+
+ggplot(data= masses, aes(x=mass.g., y= newborn.g.))+
+  geom_point()+
+  labs(x="Log(Adult Mass)", y="Log(Newborn Mass)")+
+  scale_x_log10()+
+  scale_y_log10()+
+  facet_wrap(~order)
+
+ggplot(data= masses, aes(x=mass.g., y= newborn.g.))+
+  geom_point()+
+  geom_smooth(method = "lm")+
+  labs(x="Log(Adult Mass)", y="Log(Newborn Mass)")+
+  scale_x_log10()+
+  scale_y_log10()+
+  facet_wrap(~order)
+
+
